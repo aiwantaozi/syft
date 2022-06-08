edfa2a35a98f5144246d666cd7fc6dd7242721e8
thxCode <thxcode0824@gmail.com>
2022-06-08 16:00:55 +0800

    feat: gen sbom via java build tool
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- internal/config/pkg.go
+++ internal/config/pkg.go
@@ -1,19 +1,20 @@
 package config
 
 import (
-	"github.com/spf13/viper"
-
 	"github.com/anchore/syft/syft/pkg/cataloger"
+	"github.com/spf13/viper"
 )
 
 type pkg struct {
-	Cataloger               catalogerOptions `yaml:"cataloger" json:"cataloger" mapstructure:"cataloger"`
-	SearchUnindexedArchives bool             `yaml:"search-unindexed-archives" json:"search-unindexed-archives" mapstructure:"search-unindexed-archives"`
-	SearchIndexedArchives   bool             `yaml:"search-indexed-archives" json:"search-indexed-archives" mapstructure:"search-indexed-archives"`
+	Cataloger               catalogerOptions  `yaml:"cataloger" json:"cataloger" mapstructure:"cataloger"`
+	SearchByBuildTools      buildtoolsOptions `yaml:"search-by-buildtools" json:"search-by-buildtools" mapstructure:"search-by-buildtools"`
+	SearchUnindexedArchives bool              `yaml:"search-unindexed-archives" json:"search-unindexed-archives" mapstructure:"search-unindexed-archives"`
+	SearchIndexedArchives   bool              `yaml:"search-indexed-archives" json:"search-indexed-archives" mapstructure:"search-indexed-archives"`
 }
 
 func (cfg pkg) loadDefaultValues(v *viper.Viper) {
 	cfg.Cataloger.loadDefaultValues(v)
+	cfg.SearchByBuildTools.loadDefaultValues(v)
 	c := cataloger.DefaultSearchConfig()
 	v.SetDefault("package.search-unindexed-archives", c.IncludeUnindexedArchives)
 	v.SetDefault("package.search-indexed-archives", c.IncludeIndexedArchives)
@@ -21,4 +22,16 @@ func (cfg pkg) loadDefaultValues(v *viper.Viper) {
 
 func (cfg *pkg) parseConfigValues() error {
 	return cfg.Cataloger.parseConfigValues()
+}
+
+func (cfg pkg) ToConfig() cataloger.Config {
+	return cataloger.Config{
+		Search: cataloger.SearchConfig{
+			ByBuildTools:             cfg.SearchByBuildTools.Enabled,
+			ByBuildToolsWithMode:     cfg.SearchByBuildTools.Mode,
+			IncludeIndexedArchives:   cfg.SearchIndexedArchives,
+			IncludeUnindexedArchives: cfg.SearchUnindexedArchives,
+			Scope:                    cfg.Cataloger.ScopeOpt,
+		},
+	}
 }
